afed2dd3da3ed468d849675aa0ca63c5a8ec913e
Anton <anton.matrenin@introduct.tech>
2020-08-06 20:10:22 +0300

    Small Fixes

--- src/components/CellsWrapper.tsx
+++ src/components/CellsWrapper.tsx
@@ -1,13 +1,11 @@
 import styled from 'styled-components';
 import { CELL_BORDER_COLOR, CELL_SIZE } from './Cell';
-import { AREA_SIZE } from '../utils/constants';
 
 export const CellsWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(10, ${CELL_SIZE}px);
   list-style: none;
   padding: 0;
-  width: ${CELL_SIZE * AREA_SIZE}px;
   border-top: 1px solid ${CELL_BORDER_COLOR};
   border-left: 1px solid ${CELL_BORDER_COLOR};
   margin-left: 40px;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,14 +5,6 @@ export const SIMULATION_DELAY_MS = 100;
 export const shipModelsList = [
   ShipModel.CURVED,
   ShipModel.LINE,
-  ShipModel.LINE,
-  ShipModel.LINE,
-  ShipModel.LINE,
-  ShipModel.LINE,
-  ShipModel.LINE,
-  ShipModel.LINE,
-  ShipModel.LINE,
-  ShipModel.LINE,
   ShipModel.SINGLE,
   ShipModel.SINGLE,
 ];
